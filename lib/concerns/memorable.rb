624694bafd1f63d401995e96b0787ff5ddde5ae2
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-05 23:26:41 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,2 +1,20 @@
 module Memorable
-end 
+  module ClassMethods
+
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
+  end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+  end
+end
