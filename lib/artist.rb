ec4ed0b3ca7cdf332ad5fe1862c5d65d6a770128
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-05 23:22:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,10 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-
+extend Memorable::ClassMethods
+include Memorable::InstanceMethods
+extend Findable
+include PARAMABLE 
   @@artists = []
 
   def initialize
