7e89d97b2280224594e60a94f7a6fe2821c25b28
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-06 00:12:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,9 +10,9 @@ include PARAMABLE
   @@artists = []
 
   def initialize
-    @@artists << self
-    @songs = []
-  end
+#    @@artists << self
+#    @songs = []
+#  end
 
   #def self.find_by_name(name)
   #  @@artists.detect{|a| a.name == name}
