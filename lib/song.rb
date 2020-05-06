3130eb6711b98e52f084d641d462fd5a3185a71e
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-06 00:37:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   extend Memorable::ClassMethods
     include Memorable::InstanceMethods
     extend Findable
-    include paramable
+    include Paramable
   @@songs = []
 
   #def initialize
